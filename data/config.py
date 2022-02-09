7df9824be8e9f7f4f0246f1483c0ed733b188bc4
Stkos95 <plstkos@yandex.ru>
2022-02-09 14:39:47 +0300

    исправлена клавиатура и подсчет результата

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 from environs import Env
 import json
-
+import requests
 
 env=Env()
 env.read_env()
@@ -16,7 +16,11 @@ lang = 'ru'
 link_city='http://api.openweathermap.org/geo/1.0/direct'
 
 '''Для викторины'''
-token_quiz=env.str('TOKEN_QUIZ')
+# token_quiz=env.str('TOKEN_QUIZ')
 link_quiz=env.str("LINK_QUIZ")
 
 
+# link_token=env.str('TOKEN_LINK')
+# request = requests.get(url=link_quiz).json()
+# token_quiz = request['token']
+link_token ='https://opentdb.com/api_token.php?command=request'
--- handlers/quiz.py
+++ handlers/quiz.py
@@ -1,25 +1,29 @@
 from loader import dp
 from aiogram import types
 import requests
-import json
-from data.config import token_quiz, link_quiz
+from data.config import  link_quiz, link_token
 from FSM.states import QuizGame
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.dispatcher import FSMContext
 import random
+
+
 class Questions:
     def __init__(self, question, correct_answer, incorrect_answer):
         self.question = question
         self.correct_answer = correct_answer
         self.incorrect_answer = incorrect_answer
 
+def token_url(url=link_token):
+    return requests.get(url).json()
 
-def quiz(amount='10', difficulty='easy',type='boolean'):
+def quiz(token,amount='10', category='21',type='boolean'):
     return requests.get(url=link_quiz, params=locals()).json()
 
 @dp.callback_query_handler(text='quiz', state=None)
 async def quizing(call: types.CallbackQuery, state: FSMContext):
-    question_data = quiz()
+    token_quiz = token_url()['token']
+    question_data = quiz(token_quiz)
     questions_bank = []
     for i in question_data["results"]:
         new_question = Questions(i['question'],i['correct_answer'],i['incorrect_answers'][0])
@@ -29,7 +33,7 @@ async def quizing(call: types.CallbackQuery, state: FSMContext):
 
     await QuizGame.quiz_1.set()
     async with state.proxy() as data:
-        data['number'] = 0
+        data['number'] = 1
         data['score'] = 0
         data['question_bank'] = questions_bank
 
@@ -45,41 +49,48 @@ async def quizing(call: types.CallbackQuery, state: FSMContext):
                                                                                          ]
                                                                                      ]))
 
-@dp.callback_query_handler(text='incorrect',state=QuizGame.quiz_1)
+@dp.callback_query_handler(text=['incorrect'],state=QuizGame.quiz_1)
 async def quiz_answers(call: types.CallbackQuery, state: FSMContext):
     await call.answer()
-    switch_kb = random.randint(1,2)
     async with state.proxy() as data:
         if data['number'] < len(data['question_bank'])-1:
-            data['number'] +=1
+
             await call.message.answer(f'Вы ответили НЕПРАВИЛЬНО\n'
                                   f'Ваш счет {data["score"]}/{data["number"]}')
             question = data['question_bank'][data['number']].question
             correct_answer = data['question_bank'][data['number']].correct_answer
             incorrect_answer = data['question_bank'][data['number']].incorrect_answer
-            await call.message.answer(text=f'{question}({correct_answer})', reply_markup=InlineKeyboardMarkup(row_width=2,
-                                                                                                              inline_keyboard=[
-                                                                                                                  [
-                                                                                                                      InlineKeyboardButton(
-                                                                                                                          text=correct_answer,
-                                                                                                                          callback_data='correct'),
-                                                                                                                      InlineKeyboardButton(
-                                                                                                                          text=incorrect_answer,
-                                                                                                                          callback_data='incorrect')
-                                                                                                                  ]
-                                                                                                              ] if switch_kb ==1 else [
-                                                                                                                  [
-                                                                                                                      InlineKeyboardButton(
-                                                                                                                          text=incorrect_answer,
-                                                                                                                          callback_data='incorrect'),
-                                                                                                                      InlineKeyboardButton(
-                                                                                                                          text=correct_answer,
-                                                                                                                          callback_data='correct')
-                                                                                                                  ]
-                                                                                                              ] ))
+            data['number'] += 1
+            if random.randint(1,2) == 1:
+                await call.message.answer(text=f'{question}({correct_answer})', reply_markup=InlineKeyboardMarkup(row_width=2,
+                                                                                                                  inline_keyboard=[
+                                                                                                                      [
+                                                                                                                          InlineKeyboardButton(
+                                                                                                                              text=correct_answer,
+                                                                                                                              callback_data='correct'),
+                                                                                                                          InlineKeyboardButton(
+                                                                                                                              text=incorrect_answer,
+                                                                                                                              callback_data='incorrect')
+                                                                                                                      ]
+                                                                                                                  ] ))
+            else:
+                await call.message.answer(text=f'{question}({correct_answer})',
+                                          reply_markup=InlineKeyboardMarkup(row_width=2,
+                                                                            inline_keyboard=[
+                                                                                [
+                                                                                    InlineKeyboardButton(
+                                                                                        text=incorrect_answer,
+                                                                                        callback_data='incorrect'),
+                                                                                    InlineKeyboardButton(
+                                                                                        text=correct_answer,
+                                                                                        callback_data='correct')
+                                                                                ]
+                                                                            ]))
+
             await call.message.edit_reply_markup(reply_markup=None)
         else:
-            await call.message.answer('Вы завершили викторину!')
+            await call.message.answer(f'Вы завершили викторину!\n'
+                                      f'Ваш счет: {data["score"]}/{data["number"]}')
             await state.finish()
 
 
@@ -92,14 +103,18 @@ async def quiz_answers(call: types.CallbackQuery, state: FSMContext):
     await call.answer()
     switch_kb = random.randint(1,2)
     async with state.proxy() as data:
-        if data['number'] < len(data['question_bank'])-1:
-            data['number'] += 1
-            data['score'] += 1
+        data['score'] += 1
+        if data['number'] < len(data['question_bank']):
+
+
+            print(data['number'])
             await call.message.answer(f'Вы ответили правильно\n'
                                   f'Ваш счет {data["score"]}/{data["number"]}')
+
             question = data['question_bank'][data['number']].question
             correct_answer = data['question_bank'][data['number']].correct_answer
             incorrect_answer = data['question_bank'][data['number']].incorrect_answer
+            data['number'] += 1
             await call.message.answer(text=f'{question}({correct_answer})', reply_markup=InlineKeyboardMarkup(row_width=2,
                                                                                                               inline_keyboard=[
                                                                                                                   [
@@ -122,7 +137,8 @@ async def quiz_answers(call: types.CallbackQuery, state: FSMContext):
                                                                                                               ] ))
             await call.message.edit_reply_markup(reply_markup=None)
         else:
-            await call.message.answer('Вы завершили викторину!')
+            await call.message.answer(f'Вы завершили викторину!\n'
+                                      f'Ваш счет: {data["score"]}/{data["number"]}')
             await state.finish()
 
 
